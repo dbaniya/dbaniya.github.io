5dea1f52ec5c343238b330f102e0a2de9d1a57f0
dbaniya <dbaniya@tcioe.edu.np>
2023-11-15 05:36:42 +0545

    changes

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,10 +3,14 @@ $(document).ready(function() {
   $('a.abstract').click(function() {
     $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
     $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".video.hidden.open").toggleClass('open');
+
   });
   $('a.video').click(function() {
     $(this).parent().parent().find(".video.hidden").toggleClass('open');
     $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
+
   });
 
   $('a.video-1').click(function() {
@@ -22,7 +26,10 @@ $(document).ready(function() {
     $('a.bibtex').click(function() {
     $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".video.hidden.open").toggleClass('open');
+
   });
+
   $('a').removeClass('waves-effect waves-light');
 
   // bootstrap-toc
